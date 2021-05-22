3a630b78acfd2b0e974f142bb4948809b1c835ca
EC2 Default User <ec2-user@ip-172-31-0-57.ec2.internal>
2021-05-22 03:16:16 +0000

    [Add]商品登録の一部

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -6,8 +6,20 @@ class Admin::ItemsController < ApplicationController
   end
 
   def new
+    #@item_image = ItemImage.new
+    @item = Item.new
+  end
+
+  def create
+    #@item_image = ItemImage.new
+    #@item_image.save
+    #redirect_to admin_items_path
+    @item = Item.new
+    @item.save
+    redirect_to admin_items_path
   end
 
   def show
   end
+
 end
