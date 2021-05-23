3a6c5bd1dd4f7713c2e98607c7f351faf1f295b7
EC2 Default User <ec2-user@ip-172-31-0-57.ec2.internal>
2021-05-23 07:38:43 +0000

    [Add]refileのシークレットキーの追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'f5bfc7230c48180919e346fd08e23cb17812ff00c7fd211b8ea0ee71b295e1ad56566444a1fb615d9438c292e011502a36607eb44d6eb1192d4c444500a134dc'
