4cb42f482e7f10ed6b7a43dc3034b89d8b83662f
EC2 Default User <ec2-user@ip-172-31-0-57.ec2.internal>
2021-05-24 06:37:01 +0000

    [Add]

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,18 @@ devise_for :customers, controllers: {
   passwords:     'customers/passwords',
   registrations: 'customers/registrations'
 }
+ namespace :public do
+  root to: 'homes#top'
+  get '/about' => 'homes#about'
+  resources :items, only: [:index, :show]
+
+ end
+
 namespace :admin do
   resources :items, except: [:create]
   post '/items/new' => 'items#create'
+  resources :genres, only: [:index, :new, :edit, :update]
+  post '/genres/new' => 'genres#create'
 end
 
 end
