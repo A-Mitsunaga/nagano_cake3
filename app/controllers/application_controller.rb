4dfdeaff786e405cd2492bdd88ef976a58032a74
EC2 Default User <ec2-user@ip-172-31-0-57.ec2.internal>
2021-05-24 07:42:10 +0000

    rollback直前

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
 # strong parameter
 before_action :configure_permitted_parameters, if: :devise_controller?
 
+# def after_sign_in_path_for(resource)
+    # public_items_path
+  # end
+
+protected
 def configure_permitted_parameters
   devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
 end
