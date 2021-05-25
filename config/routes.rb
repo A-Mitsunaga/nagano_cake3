b553cf87805e443fc0e3e88b090d5a709746ca1b
EC2 Default User <ec2-user@ip-172-31-0-57.ec2.internal>
2021-05-25 01:49:47 +0000

    [Add]publicからhomesを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-root :to => 'public/homes#top'
-get '/about' => 'public/homes#about'
+root :to => 'homes#top'
+get '/about' => 'homes#about'
 devise_for :admins, controllers: {
   sessions:      'admins/sessions',
   passwords:     'admins/passwords',
