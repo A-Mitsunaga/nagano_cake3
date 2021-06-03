aabc7a0f3342ff7067acf8f326b46c580076a7ee
EC2 Default User <ec2-user@ip-172-31-0-57.ec2.internal>
2021-06-03 06:25:10 +0000

    [Add]メンター質問後

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,5 @@
 Rails.application.routes.draw do
 
-root :to => 'homes#top'
-get '/about' => 'homes#about'
-get '/customers/my_page' => 'customers#show'
-get '/customers/unsubscribe' => 'customers#unsubscribe'
-patch '/customers/withdrawal' => 'customers#withdrawal'
-
-resources :customers, only: [:edit, :update]
-resources :items, only: [:index, :show]
-
-resources :orders, only: [:new, :create, :index, :show]
-post '/orders/confirm' => 'orders#confirm'
-get '/orders/thanks' => 'orders#thanks'
-
-resources :order_items, only: [:index, :edit, :create, :update, :destroy]
-
-resources :cart_items, only: [:index, :update, :create]
-delete '/cart_items/:id' => 'cart_items#destroy'
-delete '/cart_items/destroy_all' => 'cart_items#destroy_all'
-
 devise_for :admins, controllers: {
   sessions:      'admins/sessions',
   passwords:     'admins/passwords',
@@ -31,12 +12,32 @@ devise_for :customers, controllers: {
   registrations: 'customers/registrations'
 }
 
+root :to => 'homes#top'
+get '/about' => 'homes#about'
+
+resource :customers, only: [:edit, :update]
+get '/customers/my_page' => 'customers#show'
+get '/customers/unsubscribe' => 'customers#unsubscribe'
+patch '/customers/withdrawal' => 'customers#withdrawal'
+
+resources :items, only: [:index, :show]
+post '/orders/confirm' => 'orders#confirm'
+get '/orders/thanks' => 'orders#thanks'
+
+resources :orders, only: [:new, :create, :index, :show]
+
+resources :order_items, only: [:index, :edit, :create, :update, :destroy]
+
+resources :cart_items, only: [:index, :update, :create]
+delete '/cart_items/:id' => 'cart_items#destroy'
+delete '/cart_items/destroy_all' => 'cart_items#destroy_all'
+
+
 #post '/customers/sign_up' => 'customers/registrations#create'
 
 namespace :admin do
   resources :items, except: [:create]
   post '/items/new' => 'items#create'
-  post '/genres/new' => 'genres#create'
   resources :customers, only: [:index, :show, :edit, :update]
   resources :orders, only: [:show, :update]
   get '/' => 'homes#top'
