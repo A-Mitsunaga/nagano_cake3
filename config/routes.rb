8804181e1e110402a7d288f9bf4ac57e07ec6c77
EC2 Default User <ec2-user@ip-172-31-0-57.ec2.internal>
2021-06-05 06:21:53 +0000

    [Add]address

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,22 @@
 Rails.application.routes.draw do
 
-devise_for :admins, controllers: {
-  sessions:      'admins/sessions',
-  passwords:     'admins/passwords',
-  registrations: 'admins/registrations'
-}
-
-devise_for :customers, controllers: {
-  sessions:      'customers/sessions',
-  passwords:     'customers/passwords',
-  registrations: 'customers/registrations'
-}
-
 root :to => 'homes#top'
 get '/about' => 'homes#about'
 
 resource :customers, only: [:edit, :update]
 get '/customers/my_page' => 'customers#show'
+patch 'customers/edit' => 'customers#update'
 get '/customers/unsubscribe' => 'customers#unsubscribe'
 patch '/customers/withdrawal' => 'customers#withdrawal'
 
 resources :items, only: [:index, :show]
+
 post '/orders/confirm' => 'orders#confirm'
 get '/orders/thanks' => 'orders#thanks'
-
 resources :orders, only: [:new, :create, :index, :show]
 
-resources :order_items, only: [:index, :edit, :create, :update, :destroy]
+#resources :order_items, only: [:index, :edit, :create, :update, :destroy]
+resources :addresses, only: [:index, :edit, :create, :update, :destroy]
 
 resources :cart_items, only: [:index, :update, :create]
 delete '/cart_items/:id' => 'cart_items#destroy'
@@ -43,5 +33,16 @@ namespace :admin do
   get '/' => 'homes#top'
 end
 
+devise_for :admins, controllers: {
+  sessions:      'admins/sessions',
+  passwords:     'admins/passwords',
+  registrations: 'admins/registrations'
+}
+
+devise_for :customers, controllers: {
+  sessions:      'customers/sessions',
+  passwords:     'customers/passwords',
+  registrations: 'customers/registrations'
+}
 
 end
