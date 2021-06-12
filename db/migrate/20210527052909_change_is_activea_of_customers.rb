272b6707c92166c695af94f11bb20f11343431e6
EC2 Default User <ec2-user@ip-172-31-0-57.ec2.internal>
2021-06-12 08:30:24 +0000

    [Add]退会処理

--- db/migrate/20210527052909_change_is_activea_of_customers.rb
+++
@@ -1,9 +0,0 @@
-class ChangeIsActiveaOfCustomers < ActiveRecord::Migration[5.2]
-  def up
-    change_column :customers, :is_active, :boolean, default: 'TRUE'
-  end
-
-  def down
-    change_column :customers, :is_active, :boolean
-  end
-end
